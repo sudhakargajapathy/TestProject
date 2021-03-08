e69a35792def1f95ceb93f5b87985c0c7e7f2d24
Sudhakar Gajapathy <sudhakargajapathy@outlook.com>
2021-03-08 12:27:57 -0700

    intial version

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -6,7 +6,18 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
+            int number = 1;
+            char alphabet = 'A';
+            string firstName = "Sudhakar";
+            var lastName = "Gajapathy";
+            float price = 10.25f;
+
             Console.WriteLine("Hello World!  ~From Sudhakar");
+            Console.WriteLine(number);
+            Console.WriteLine(alphabet);
+            Console.WriteLine(firstName);
+            Console.WriteLine("{0} {1}", firstName, lastName);
+            Console.WriteLine(price);
         }
     }
 }
